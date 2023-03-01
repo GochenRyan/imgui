e935df299a5b9bcf7f4525971be4dbd6699c4147
GochenRyan <rangaocheng@outlook.com>
2023-03-01 18:14:37 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,23 @@
-target("ImGui")
+target("libImGui")
     set_kind("static")
-    -- set_targetdir("$(projectdir)/libs/ImGui")
-    add_headerfiles("*.h")
-    add_files("*.cpp")
-    set_group("Dependencies")
+    add_includedirs(
+        "$(projectdir)/Vendor/glfw/include/",
+        -- inner
+        "./"
+    )
+    add_deps(
+        "libGLFW"
+    )
+
+    add_headerfiles(
+        "*.h",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.h"
+    )
+    add_files(
+        "*.cpp",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp"
+    )
+
+    add_linkdirs("$(projectdir)/lib/")
