627669d809bbbaff5cd9b62a39bef6199d7f79c5
GochenRyan <rangaocheng@outlook.com>
2023-04-11 18:08:48 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,21 +1,30 @@
 target("libImGui")
     set_kind("static")
+    add_defines("IMGUI_ENABLE_FREETYPE")
     add_includedirs(
         "$(projectdir)/Vendor/glfw/include/",
+        "$(projectdir)/Vendor/freetype/include/",
         -- inner
         "./"
     )
     add_deps(
-        "libGLFW"
+        "libGLFW",
+        "libFreeType"
+    )
+
+    add_links(
+        "libFreeType"
     )
 
     add_headerfiles(
         "*.h",
+        "misc/freetype/*.h",
         "backends/imgui_impl_glfw.h",
         "backends/imgui_impl_opengl3.h"
     )
     add_files(
         "*.cpp",
+        "misc/freetype/*.cpp",
         "backends/imgui_impl_glfw.cpp",
         "backends/imgui_impl_opengl3.cpp"
     )
